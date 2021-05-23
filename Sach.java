e3d0a763fd64ad710977d9b94e10ba4b3b8ab4c0
sytruong2001 <78136732+sytruong2001@users.noreply.github.com>
2021-05-23 22:16:40 +0700

    project_oop

---
+++ Sach.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1_oop;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Sach {
+    private String maSach, nhaXuatBan;
+    private double donGia;
+    private int soLuong;
+    Scanner scanner = new Scanner(System.in);
+     
+    public Sach() {
+        super();
+    }
+ 
+    public Sach(String maSach, String nhaXuatBan, double donGia, int soLuong) {
+        super();
+        this.maSach = maSach;
+        this.nhaXuatBan = nhaXuatBan;
+        this.donGia = donGia;
+        this.soLuong = soLuong;
+    }
+ 
+    public String getMaSach() {
+        return maSach;
+    }
+ 
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+ 
+    public String getNhaXuatBan() {
+        return nhaXuatBan;
+    }
+ 
+    public void setNhaXuatBan(String nhaXuatBan) {
+        this.nhaXuatBan = nhaXuatBan;
+    }
+ 
+    public double getDonGia() {
+        return donGia;
+    }
+ 
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+ 
+    public int getSoLuong() {
+        return soLuong;
+    }
+ 
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+ 
+    public void nhapSach() {
+        System.out.print("Nhập mã sách: ");
+        maSach = scanner.nextLine();
+        System.out.print("Nhập tên nhà xuất bản: ");
+        nhaXuatBan = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        donGia = scanner.nextDouble();
+        System.out.print("Nhập số lượng: ");
+        soLuong = scanner.nextInt();
+    }
+     
+    @Override
+    public String toString() {
+        return "Mã sách: " + this.maSach + ", tên nhà xuất bản: " + this.nhaXuatBan + ", đơn giá: " + 
+                this.donGia + ", số lượng: " + this.soLuong;
+    }
+}
